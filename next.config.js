1dedf5a869d437f75572ae16fd2f8e7db2fc302c
Mert Sakınç <63242329+mertsak@users.noreply.github.com>
2023-06-06 23:43:45 +0300

    cors

--- next.config.js
+++ next.config.js
@@ -20,4 +20,15 @@ const nextConfig = {
   },
 };
 
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://api.example.com/:path*",
+      },
+    ];
+  },
+};
+
 module.exports = nextConfig;
